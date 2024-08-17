652eaf405957cb0eef223e7c2adf6315307c54c4
ollieri3 <oliver.ridsdale@gmail.com>
2024-08-17 17:59:42 +0100

    Add Mona Sans font and set as default sans font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+
+import defaultTheme from "tailwindcss/defaultTheme";
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+      fontFamily: {
+        sans: ["Mona Sans", ...defaultTheme.fontFamily.sans],
+        local: [
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica",
+          "Arial",
+          "sans-serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+        ],
+      },
+    },
 	},
 	plugins: [],
 }
